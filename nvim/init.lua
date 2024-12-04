ec6b2dcd4d87f18202028f48d9f20ac8af9054f5
eliminator <eli.entrich@gmail.com>
2024-12-04 20:28:45 +0100

    setup nvim with lazy, lsp, telescope, treesitter and nord-colorscheme

---
+++ nvim/init.lua
@@ -0,0 +1,3 @@
+-- Setup lazy.nvim
+require('config/lazy')
+print("Lazy setup complete")
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,5 @@
+vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
+vim.opt.number = true       -- Show line numbers
+vim.opt.expandtab = true    -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4      -- Shift 4 spaces when tabbing
+vim.opt.tabstop = 4         -- 1 tab == 4 spaces
