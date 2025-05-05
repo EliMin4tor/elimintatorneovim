9ed5e648ce2b156f6b63e1c961bae25f1d09e581
eliminator <eli.entrich@gmail.com>
2025-05-05 22:51:57 +0200

    fix: configure messages to be shown in bottom right corner

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -194,12 +194,49 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
+      cmdline = {
+        enabled = true, -- enables the Noice cmdline UI
+        view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+        opts = {}, -- global options for the cmdline. See section on views
+        ---@type table<string, CmdlineFormat>
+        format = {
+          -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+          -- view: (default is cmdline view)
+          -- opts: any options passed to the view
+          -- icon_hl_group: optional hl_group for the icon
+          -- title: set to anything or empty string to hide
+          cmdline = { pattern = "^:", icon = "", lang = "vim" },
+          search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+          filter = false, -- { pattern = "^:%s*!", icon = "$", lang = "bash" },
+          lua = false, --{ pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+          help = false, -- { pattern = "^:%s*he?l?p?%s+", icon = "" },
+          input = { view = "cmdline_input", icon = "󰥻 " }, -- Used by input()
+          -- lua = false, -- to disable a format, set to `false`
+        },
+      },
+      -- messages view options:
+      --      messages            new window and select
+      --      split               new window
+      --      cmdline_popup       in commandline - stays long
+      --      notify              as message icon right
+      --      virtualtext         message as ghost text in line
+      --      mini                corner bottom right
+      messages = {
+        enabled = true, -- enables the Noice messages UI
+        view = "mini", -- default view for messages
+        view_error = "mini", -- view for errors
+        view_warn = "mini", -- view for warnings
+        view_history = "messages", -- view for :messages
+        view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+      },
       lsp = {
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true,
         },
+        messages = { view = "mini" },
       },
       routes = {
         {
@@ -211,15 +248,12 @@ return {
               { find = "; before #%d+" },
             },
           },
-          -- view = "messages", -- new window and select
-          -- view = "split", -- new window
-          -- view = "cmdline_popup", -- in commandline - stays long
-          -- view = "notify", -- as message icon right
+          opts = { timeout = 8000 }, -- Time in milliseconds (3 seconds)
           view = "mini", -- bottom right
         },
       },
       presets = {
-        bottom_search = true,
+        bottom_search = false,
         command_palette = true,
         long_message_to_split = true,
       },
